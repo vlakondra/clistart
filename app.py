f6443b4897bb735dcba8c780f5086188f56a2836
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-04-11 21:19:17 +0500

    start work

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
-(a,b) =range(2)
-print(a,b)
+import os,sys
+
+# print(__file__, os.path.dirname( os.path.abspath(__file__) ) )
+# print(" == ",os.path.dirname(os.path.pardir))
+print(os.path.abspath(__file__))
+print(os.path.dirname(__file__))
+print(os.path.basename(__file__))
+
+print(os.path.split(os.path.realpath(__file__)))
+# = os.path.dirname + os.path.basename
+
+sys.path.append(os.path.pardir)  #('/home/vkondra/MyDevs/clistart')
+
+print(sys.path)
+
+
+from clistart import __app_name__, cli
+print(__app_name__)
+def main():
+    cli.app(prog_name=__app_name__)
+
+if __name__ == "__main__":
+    main()
